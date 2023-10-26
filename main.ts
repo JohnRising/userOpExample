22481598e8ad5a3de0242741624853b05a3c5330
JohnRising <john@risingendeavours.com>
2023-10-26 11:45:54 -0700

    Add comments

--- main.ts
+++ main.ts
@@ -1,12 +1,30 @@
+// Welcome to the ERC-4337 tutorial #1!
+// This tutorial walks you though a simple ERC-4337 transaction: sending a User Operation
+// with gas paid by a Paymaster.
+//
+// You can view more information about this tutorial at
+// https://docs.stackup.sh/docs/get-started-with-stackup
+//
+
+// This example uses the userop.js library to build the transaction, but you can use any
+// library.
 import * as dotenv from "dotenv";
 import { ethers } from "ethers";
 import { Presets, Client } from "userop";
 
+// DO THIS FIRST
+//
+// Copy the example environment file `.env.example` to `.env` and add the variables.
+// You can get a free API key at https://app.stackup.sh/, but any ERC-4337 bundler or
+// paymaster service should work.
 dotenv.config();
 const signingKey = process.env.SIGNING_KEY || "";
 const rpcUrl = process.env.RPC_URL || "";
 const paymasterUrl = process.env.PAYMASTER_URL || "";
 
+// This function creates the call data that will be executed. This combines the approval
+// and send call in a single transaction. You can add as many contract calls as you want
+// in a User Operation.
 async function approveAndSendToken(
   to: string,
   token: string,
@@ -33,15 +51,20 @@ async function approveAndSendToken(
   return [approve, send];
 }
 
+// This function builds the User Operation and sends it to the blockchain via the ERC-4337
+// bundler network.
 async function main() {
-  // Paymaster
+  // Define the kind of paymaster you want to use. If you do not want to use a paymaster,
+  // comment out these lines.
   const paymasterContext = { type: "payg" };
   const paymasterMiddleware = Presets.Middleware.verifyingPaymaster(
     paymasterUrl,
     paymasterContext
   );
 
-  // Initialize userop builder
+  // Initialize the User Operation
+  // Userop.js has a few presets for different smart account types to set default fields
+  // for user operations. This uses the ZeroDev Kernel contracts.
   const signer = new ethers.Wallet(signingKey);
   var builder = await Presets.Builder.Kernel.init(signer, rpcUrl, {
     paymasterMiddleware: paymasterMiddleware,
@@ -49,18 +72,22 @@ async function main() {
   const address = builder.getSender();
   console.log(`Account address: ${address}`);
 
-  // Approve and send token
+  // Create the call data
   const to = address;
   const token = "0x3870419Ba2BBf0127060bCB37f69A1b1C090992B";
   const value = "0";
   const calls = await approveAndSendToken(to, token, value);
 
-  // Build & send
+  // Send the User Operaiton to the ERC-4337 mempool
   const client = await Client.init(rpcUrl);
   const res = await client.sendUserOperation(builder.executeBatch(calls), {
     onBuild: (op) => console.log("Signed UserOperation:", op),
   });
 
+  // Print the results!
+  // If you encounter any errors, search the FAQ at https://docs.stackup.sh/docs/faq or
+  // contact us in Discord at https://discord.com/invite/FpXmvKrNed or
+  // email us at support@stackup.sh
   console.log(`UserOpHash: ${res.userOpHash}`);
   console.log("Waiting for transaction...");
   const ev = await res.wait();
