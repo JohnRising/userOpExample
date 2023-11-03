24575c2491314c921bcc0c310831e75835ba4df9
johnrising <john@risingendeavours.com>
2023-11-03 14:14:27 -0700

    clean up

--- main.ts
+++ main.ts
@@ -9,42 +9,12 @@
 
 // This example uses the userop.js library to build the transaction, but you can use any
 // library.
+
 import { ethers } from "ethers";
 import { Presets, Client } from "userop";
 
-const signingKey = "0x4337433743374337433743374337433743374337433743374337433743374337";
 const rpcUrl ="https://public.stackup.sh/api/v1/node/ethereum-sepolia";
-const paymasterUrl = ""; // Optional
-
-// This function creates the call data that will be executed. This combines the approval
-// and send call in a single transaction. You can add as many contract calls as you want
-// in a User Operation.
-async function approveAndSendToken(
-  to: string,
-  token: string,
-  value: string
-): Promise<any[]> {
-  const ERC20_ABI = require("./erc20Abi.json");
-  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
-  const erc20 = new ethers.Contract(token, ERC20_ABI, provider);
-  const decimals = await Promise.all([erc20.decimals()]);
-  const amount = ethers.utils.parseUnits(value, decimals);
-
-  const approve = {
-    to: token,
-    data: erc20.interface.encodeFunctionData("approve", [to, amount]),
-  };
-
-  const send = {
-    to: token,
-    data: erc20.interface.encodeFunctionData("transfer", [to, amount]),
-  };
-
-  const callTargets = [approve.to, send.to];
-  const callData = [approve.data, send.data];
-
-  return [callTargets, callData];
-}
+const paymasterUrl = ""; // Optional - you can get one at https://app.stackup.sh/
 
 async function main() {
   const paymasterContext = { type: "payg" };
@@ -52,37 +22,47 @@ async function main() {
     paymasterUrl,
     paymasterContext
   );
-
-  // Initialize the User Operation
-  const signer = new ethers.Wallet(signingKey);
   const opts = paymasterUrl === "" ? {} : {
     paymasterMiddleware: paymasterMiddleware,
   }
+
+  // Initialize the account
+  const signingKey = "0x4337433743374337433743374337433743374337433743374337433743374337";
+  const signer = new ethers.Wallet(signingKey);
   var builder = await Presets.Builder.SimpleAccount.init(signer, rpcUrl, opts);
   const address = builder.getSender();
   console.log(`Account address: ${address}`);
 
   // Create the call data
-  const to = address;
-  const token = "0x3870419Ba2BBf0127060bCB37f69A1b1C090992B";
-  const value = "0";
-  const [callTargets, callData] = await approveAndSendToken(to, token, value);
+  const to = address; // Receiving address, in this case we will send it to ourselves
+  const token = "0x3870419Ba2BBf0127060bCB37f69A1b1C090992B"; // Address of the ERC-20 token
+  const value = "0"; // Amount of the ERC-20 token to transfer
+
+  // Read the ERC-20 token contract
+  const ERC20_ABI = require("./erc20Abi.json"); // ERC-20 ABI in json format
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+  const erc20 = new ethers.Contract(token, ERC20_ABI, provider);
+  const decimals = await Promise.all([erc20.decimals()]);
+  const amount = ethers.utils.parseUnits(value, decimals);
+
+  // Encode the calls
+  const callTo = [token, token];
+  const callData = [erc20.interface.encodeFunctionData("approve", [to, amount]),
+                    erc20.interface.encodeFunctionData("transfer", [to, amount])]
 
   // Send the User Operation to the ERC-4337 mempool
   const client = await Client.init(rpcUrl);
-  const res = await client.sendUserOperation(builder.executeBatch(callTargets, callData), {
+  const res = await client.sendUserOperation(builder.executeBatch(callTo, callData), {
     onBuild: (op) => console.log("Signed UserOperation:", op),
   });
 
-  // Print the results!
-  // If you encounter any errors, search the FAQ at https://docs.stackup.sh/docs/faq or
-  // contact us in Discord at https://discord.com/invite/FpXmvKrNed or
-  // email us at support@stackup.sh
+  // Return receipt
   console.log(`UserOpHash: ${res.userOpHash}`);
   console.log("Waiting for transaction...");
   const ev = await res.wait();
   console.log(`Transaction hash: ${ev?.transactionHash ?? null}`);
   console.log(`View here: https://jiffyscan.xyz/userOpHash/${res.userOpHash}`);
+
 }
 
 main().catch((err) => console.error("Error:", err));
